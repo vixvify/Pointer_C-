190be4edb61c8df6c1546fbb9cf2b393d51d381d
asnaweevixvify <asnaweeezor@gmail.com>
2026-02-13 10:12:33 +0700

    multiply

--- main_2.cpp
+++ main_2.cpp
@@ -31,6 +31,30 @@ void addNode(int coef, int exp,Node** head){
     newNode->prev = temp;
 }
 
+Node* checkexp(Node* headresult) {
+    Node *newresult = NULL;
+    Node *temp = headresult;
+    while (temp != NULL)
+    {
+        Node *cur = newresult;
+
+        while (cur != NULL){
+            if (cur->exp == temp->exp){
+                cur->coef += temp->coef;
+                break;
+            }
+            cur = cur->next;
+        }
+
+        if (cur == NULL){
+            addNode(temp->coef, temp->exp, &newresult);
+        }
+
+        temp = temp->next;
+    }
+    return newresult;
+}
+
 Node* addpolynomial(Node** head1,Node** head2) {
     Node *temp1 = *head1;
     Node *temp2 = *head2;
@@ -66,10 +90,21 @@ Node* addpolynomial(Node** head1,Node** head2) {
     return headresult;
 }
 
-void multiplypolynomial(int coef, int exp,Node** head) {
+Node* multiplypolynomial(Node** head1,Node** head2) {
+    Node *temp1 = *head1;
+    Node *headresult = NULL;
 
+    for(int i = 0; temp1 != NULL ; i++) {
+        Node *temp2 = *head2;
+        for(int j = 0; temp2 != NULL; j++) {
+           addNode(temp1->coef * temp2 ->coef, temp1->exp + temp2->exp,  &headresult);
+            temp2 = temp2->next;
+        }
+        temp1 = temp1->next;
+    }
+    Node* result = checkexp(headresult);
+    return result;
 }
-
 
 int main() {
     Node* head1 = NULL;
@@ -78,17 +113,24 @@ int main() {
     addNode(4,3,&head1);
     addNode(3,2,&head1);
     addNode(5,0,&head1);
-    
+
     addNode(3,5,&head2);
     addNode(5,3,&head2);
     addNode(4,2,&head2);
     addNode(6,0,&head2);
     cout  << endl;
-    Node* headresult = addpolynomial(&head1,&head2);
-    Node* temp = headresult;
-    while(temp != NULL) {
-        cout << temp -> coef << "x" << temp -> exp << endl;
-        temp = temp->next;
+    // Node* addresult = addpolynomial(&head1,&head2);
+    // Node* addtemp = addresult;
+    // while(addtemp != NULL) {
+    //     cout << addtemp -> coef << "x" << addtemp -> exp << endl;
+    //     addtemp = addtemp->next;
+    // }
+    // cout << endl;
+    Node* mulresult = multiplypolynomial(&head1,&head2);
+    Node* multemp = mulresult;
+    while(multemp != NULL) {
+        cout << multemp -> coef << "x" << multemp -> exp << endl;
+        multemp = multemp->next;
     }
     return 0;
 }
